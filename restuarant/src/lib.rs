8d5a4257870417f6d00323b6ce8776b9d677db45
youngjuning <youngjuning@aliyun.com>
2021-03-17 21:43:16 +0800

    chore: eat_at_restaurant

--- restuarant/src/lib.rs
+++ restuarant/src/lib.rs
@@ -1,15 +1,28 @@
-mod tests {
-    mod hosting {
-        fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
     }
+}
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    meal.seasonal_fruit = String::from("");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
